75fd9426e637b2cc07887df02a92a85c0115ecf5
Phil Marcus <pmarcus@reverek12.org>
2025-11-01 11:56:58 -0400

    added default parameters for Phase Zero

--- BotBattler/src/optimization/Optimizer_Phase_Zero.java
+++ BotBattler/src/optimization/Optimizer_Phase_Zero.java
@@ -4,7 +4,7 @@ import game.Battle;
 
 public class Optimizer_Phase_Zero {
 
-	private static int num = 10000;
+	private static int num = 1000;
 
 	// runs battles with increasing opponent level until player dies
 	// returns the level on which the player died
@@ -12,6 +12,7 @@ public class Optimizer_Phase_Zero {
 		int level = 1;
 		boolean isOver = false;
 		while (!isOver) {
+			//In  PHASE ZERO:
 			//Use best-guess default values for parameters 
 			//in order to generate feature scaling data
 			OptimizingChampion player = new OptimizingChampion(5,0.5, 3, 3, 0.5,
